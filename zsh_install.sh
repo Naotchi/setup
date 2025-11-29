482cfd456a2b34b84745314f7061ae54a675d264
Naotchi <n.shiba0101@gmail.com>
2025-11-29 20:52:17 +0900

    add .tmux.conf

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,1 +1,12 @@
 apt install -y zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && chmod +x install.sh && ls
+./install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && apt install zsh-syntax-highlighting
+
+echo 'export ZSH=$HOME/.oh-my-zsh' >> ~/.zshrc
+echo 'ZSH_THEME="ys"' >> ~/.zshrc
+echo 'plugins=(git zsh-autosuggestions)' >> ~/.zshrc
+echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+
+rm install.sh
