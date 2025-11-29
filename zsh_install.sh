ff926705c15857330f22f21c40cd6f8ed66ee35d
Naotchi <n.shiba0101@gmail.com>
2025-11-29 22:04:03 +0900

    feat: change tmux status bar

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,7 +1,7 @@
-apt install -y zsh
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && chmod +x install.sh && ls
+sudo apt install -y zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && chmod +x install.sh
 ./install.sh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && apt install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && apt install zsh-syntax-highlighting
 
 echo 'export ZSH=$HOME/.oh-my-zsh' >> ~/.zshrc
 echo 'ZSH_THEME="ys"' >> ~/.zshrc
